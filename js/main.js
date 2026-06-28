9a58697537802dd58ff67641671b1ee198be34bf
Fabio <fcarmouk13@gmail.com>
2026-06-28 17:08:27 -0400

    Novo evento de click e submit do formulario

--- js/main.js
+++ js/main.js
@@ -11,3 +11,28 @@ document.querySelectorAll('a[href^="#"]').forEach(anchor => {
         });
     });
 });
+
+// Acessar elementos no DOM
+const btnMsg = document.querySelector(".btnMsg");
+const inputNome = document.querySelector(".inputNome");
+const inputMsg = document.querySelector(".inputMsg");
+const areaForm = document.querySelector(".contato");
+
+btnMsg.addEventListener('click', () => {
+    console.log("clickou");
+    areaForm.classList.toggle('escondido');
+
+    if (areaForm.classList.contains('escondido')) {
+        btnMsg.textContent = 'Mensagem';
+    } else {
+        btnMsg.textContent = 'Cancelar Envio';
+    }
+});
+
+areaForm.addEventListener('submit', (event) => {
+    event.preventDefault();
+    const nome = inputNome.value;
+    console.log(nome)
+
+    areaForm.reset();
+})
